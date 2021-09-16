b84fd4f8e1224f8d1b8c5576a611cf66c701c885
Tan Hoang <zaytpotter@gmail.com>
2021-09-16 14:50:21 +0700

    a part of regex

---
+++ python/regex_essentials.py
@@ -0,0 +1,16 @@
+import re
+
+# https://developers.google.com/edu/python/regular-expressionshttps://developers.google.com/edu/python/regular-expressions
+
+text = 'test text'
+
+# Removing occurence at the start 
+re.sub(r'^some text', '', text)
+
+# Removing occurence at the end 
+re.sub(r'some text$', '', text)
+
+# Handling all date formats
+re.sub(r'\d+[\.\/-]\d+[\.\/-]\d+', '', 'Today is 14-07-2021')
+re.sub(r'\d+[\.\/-]\d+[\.\/-]\d+', '', 'Today is 14/07/2021')
+re.sub(r'\d+[\.\/-]\d+[\.\/-]\d+', '', 'Today is 14.07.2021')
