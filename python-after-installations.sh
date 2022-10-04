fa3529a1400ba8fd139ca188b9fb1286bd85dbdc
Tan Hoang <zaytpotter@gmail.com>
2022-10-04 17:22:07 +0700

    Update and rename python-after-installations.sh to python-installation.sh

--- python-after-installations.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install gcc python3-dev python3-pip libxml2-dev libxslt1-dev zlib1g-dev g++
-pip install wheel
-pip install cython
---
+++ python-installation.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install gcc python3-dev python3-pip python3-venv libxml2-dev libxslt1-dev zlib1g-dev g++
+# sudo apt install python3.x-dev  python3.x-venv 
+
+### install python 
+
+
+python3 -m pip install --upgrade pip
+pip install wheel 
+pip install setuptools
+pip install cython
